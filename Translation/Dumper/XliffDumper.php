6ce1d876006ec52135752b95dc7f7ffbc6daa778
Dieter Peeters <peetersdiet@gmail.com>
2013-09-14 02:41:36 +0200

    Fixed I/O between Xliff/Xlf formats.
    - Configured loader for xlf to be the same as for xliff.
    - Added XliffMessage to store:
    	* note-elements (children of trans-unit)
    	* attribute trans-unit('approved')
    	* attribute target('state').
    - Added I/O passthrough of existing input, e.g. XliffMessage, in Updater.php.
    - Modifications to XliffLoader and XliffDumper to use XliffMessage.
    - Added tests.

--- Translation/Dumper/XliffDumper.php
+++ Translation/Dumper/XliffDumper.php
@@ -21,6 +21,7 @@ namespace JMS\TranslationBundle\Translation\Dumper;
 use JMS\TranslationBundle\Model\FileSource;
 use JMS\TranslationBundle\JMSTranslationBundle;
 use JMS\TranslationBundle\Model\MessageCatalogue;
+use JMS\TranslationBundle\Model\Message\XliffMessage;
 
 /**
  * XLIFF dumper.
@@ -128,6 +129,9 @@ class XliffDumper implements DumperInterface
             $body->appendChild($unit = $doc->createElement('trans-unit'));
             $unit->setAttribute('id', hash('sha1', $id));
             $unit->setAttribute('resname', $id);
+            if ($message instanceof XliffMessage && $message->isApproved()) {
+                $unit->setAttribute('approved', 'yes');
+            }
 
             $unit->appendChild($source = $doc->createElement('source'));
             if (preg_match('/[<>&]/', $message->getSourceString())) {
@@ -143,8 +147,21 @@ class XliffDumper implements DumperInterface
                 $target->appendChild($doc->createTextNode($message->getLocaleString()));
             }
 
-            if ($message->isNew()) {
-                $target->setAttribute('state', 'new');
+            if ($message instanceof XliffMessage) {
+                if ($message->hasState()) {
+                    $target->setAttribute('state', $message->getState());
+                }
+                
+                if ($message->hasNotes()) {
+                    foreach ($message->getNotes() as $note) {
+                        $noteNode = $unit->appendChild($doc->createElement('note', $note['text']));
+                        if (isset($note['from'])) {
+                        	$noteNode->setAttribute('from', $note['from']);
+                        }
+                    }
+                }
+            } elseif ($message->isNew()) {
+                $target->setAttribute('state', XliffMessage::STATE_NEW);
             }
 
             if ($this->addReference) {
