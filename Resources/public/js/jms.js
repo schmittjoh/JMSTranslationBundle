f9ac8e767d32950ea20d2f6a00b2e83a4f07454b
Kate Gray <kate@codebykate.com>
2017-05-28 00:09:01 -0400

    Add copy and mass copy support between alternative translations

--- Resources/public/js/jms.js
+++ Resources/public/js/jms.js
@@ -12,6 +12,20 @@
  *      @string selector: jquery selector for domain changer fields
  *      @function handlers: event handlers to be attached to domain fields
  *
+ *  @object copier
+ *      @string selector: jquery selector for the translations that can be copied
+ *      @string copyAllLabel: label for the copy all select box
+ *      @string copyAllSelector: jquery selector for the copy all select dropdown
+ *      @string copyMessageContent: message to dynamically write in to each alternative translation
+ *      @string copySelector: jquery selector for the copy link (to bind the event handler)
+ *      @string localeSelector: jquery selector for the main translator locale dropdown
+ *      @string headerSelector: jquery selector for the header section of the UI
+ *      @function elements: adds the controls for copying into the UI
+ *      @function handlers: attaches the event handlers for copying
+ *      @function copy: copies the selected alternate translation to the active translation
+ *      @function copyClick: event handler for copying alternate translations to the current locale
+ *      @function copyAll: event handler, calls copy event for all alternate translations of the chosen locale
+ *
  *  @object truncator
  *      @string selector: jquery selector for fields to be truncated (requires Trunk8 JQuery plugin)
  *      @string side: left|right side to truncate
@@ -52,6 +66,60 @@ function JMSTranslationManager(updateMessagePath, isWritable)
             $(JMS.domain.selector).change(function() {
                 $(this).parent().submit();
             });
+        }
+    };
+
+    this.copier = {
+        selector: '.jms-alt-message',
+        copyAllLabel: 'Copy all translations from: ',
+        copyAllSelector: '.jms-copy-all-select',
+        copyMessageContent: '<a href="#" class="jms-copy-link">Copy</a>',
+        copySelector: '.jms-copy-link',
+        localeSelector: '#config select[name=locale] > option',
+        headerSelector: '.jms-trans-header',
+        elements: function (JMS) {
+            // Add the copy link to all other translations
+            $(JMS.copier.selector).prev().prepend(JMS.copier.copyMessageContent);
+
+            // Add the copy all selector to the new translation section
+            // $(JMS.copier.newMessageSelector).prepend(JMS.copier.copySelectorContent);
+            var locales=$(JMS.copier.localeSelector).clone();
+
+            // Change the current locale to a label, and make it first in the list
+            var localeSelect = $('<select/>', {
+                class: 'jms-copy-all-select'
+            });
+            localeSelect.append(locales.filter(':selected').text('[Select Locale]'));
+            localeSelect.append(locales.not(':selected'));
+
+            var selectSpan = $('<span/>', {
+                class: "jms-copy-all"
+            }).append(JMS.copier.copyAllLabel).append(localeSelect);
+            $(JMS.copier.headerSelector).prepend(selectSpan);
+        },
+        handlers: function(JMS) {
+            $(JMS.copier.copySelector).on('click', null, {"JMS": JMS}, JMS.copier.copyClick);
+            $(JMS.copier.copyAllSelector).on('change', null, {"JMS": JMS}, JMS.copier.copyAll);
+        },
+        copy: function (index, alt_translation) {
+            var translation = $(alt_translation).closest('td').prev('td').children('textarea');
+            translation.text($(alt_translation).text());
+            translation.blur();
+        },
+        copyClick: function (event) {
+            var alt_translation = $(this).closest('p').next('pre');
+            alt_translation.each (JMS.copier.copy);
+        },
+        copyAll: function (event) {
+            var JMS = event.data.JMS;
+            var elem = $(event.target);
+
+            var selectedLocale = elem.val();
+            var alt_translations = elem.closest('div').find(JMS.copier.selector).filter(
+                function() { return selectedLocale == $(this).data('locale')}
+            );
+
+            alt_translations.each(JMS.copier.copy);
         }
     };
 
@@ -173,6 +241,8 @@ function JMSTranslationManager(updateMessagePath, isWritable)
     {
         var JMS = this;
         $(document).ready(function(event) {
+            JMS.copier.elements(JMS);
+            JMS.copier.handlers(JMS);
             JMS.domain.handlers(JMS);
             JMS.truncator.truncate(JMS);
             if(JMS.isWritable)
