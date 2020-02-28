233305571c1e877eb0b64cb5800cfcdfd45a0951
Fran Moreno <franmomu@gmail.com>
2020-02-28 14:05:13 +0100

    Fix class names not corresponding to filenames

--- Tests/Translation/Extractor/File/Fixture/MyFormTypeWithDefaultDomain.php
+++ Tests/Translation/Extractor/File/Fixture/MyFormTypeWithDefaultDomain.php
@@ -22,7 +22,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class MyFormType extends AbstractType
+class MyFormTypeWithDefaultDomain extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
--- Tests/Translation/Extractor/File/Fixture/MyFormTypeWithDefaultDomainSetDefault.php
+++ Tests/Translation/Extractor/File/Fixture/MyFormTypeWithDefaultDomainSetDefault.php
@@ -22,7 +22,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class MyFormType extends AbstractType
+class MyFormTypeWithDefaultDomainSetDefault extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
