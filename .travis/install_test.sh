75c3c826ac81248b22f4a9d70e7128c3a1b7ab56
Asmir Mustafic <goetas@gmail.com>
2020-12-13 20:44:19 +0100

    there is no need for source package

--- .travis/install_test.sh
+++ .travis/install_test.sh
@@ -6,4 +6,4 @@ composer self-update --1
 
 if [[ $SYMFONY_VERSION ]]; then composer require symfony/symfony:${SYMFONY_VERSION} --no-update; fi
 
-COMPOSER_MEMORY_LIMIT=-1 composer update ${COMPOSER_FLAGS} --prefer-source --no-interaction
+COMPOSER_MEMORY_LIMIT=-1 composer update ${COMPOSER_FLAGS} --no-interaction
